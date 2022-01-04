2ee23a35283864f6bbd78fb5292ef269c9b734ae
Jhoan Carrero <carrerojhoan@gmail.com>
2022-01-04 05:32:44 -0300

    added icons at navbar

--- frontend/learning/src/components/NavBar/index.js
+++ frontend/learning/src/components/NavBar/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Container from "react-bootstrap/Container";
-import Link from "next/link";
+// import Form from "react-bootstrap/Form";
+// import FormControl from "react-bootstrap/FormControl";
+// import Button from "react-bootstrap/Button";
 
-const CustomNav = () => {
+import Link from "next/link";
+import Image from "next/image";
+
+const CustomNav = ({height="150px"}) => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Learning Cursos</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Inicio</Nav.Link>
-            
+    <Navbar style={{height:height}} fixed="top" bg="light" expand="sm">
+      <Container className="">
+        <Navbar.Brand as={Link} href="/"><a className="navbar-brand d-flex align-items-center justify-content-center gap-3"><Image src="/icon.png" width="100px" height="100px" />Learning Cursos</a></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="p-5 d-flex gap-3 align-items-center justify-content-evenly w-100">
+            <Nav.Link as={Link}  href="/"><a className="nav-link text-center" data-rr-ui-event-key="/">Inicio</a></Nav.Link>
+            <Nav.Link as={Link}  href="/cursos"><a className="nav-link text-center" data-rr-ui-event-key="/">Cursos</a></Nav.Link>
+            <Nav.Link as={Link}  href="/instructores"><a className="nav-link text-center" data-rr-ui-event-key="/">Instructores</a></Nav.Link>
+            <Nav.Link as={Link}  href="/estudiantes"><a className="nav-link text-center" data-rr-ui-event-key="/">Estudiantes</a></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
